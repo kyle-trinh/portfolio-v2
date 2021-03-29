e459ef8e648443ee1cef92979dd28299b482c00f
Binh Trinh <trinhthaibinh.ecom@gmail.com>
2021-03-28 19:10:39 -0700

    initial commit

---
+++ src/@chakra-ui/gatsby-plugin/theme.js
@@ -0,0 +1,7 @@
+import { theme as defaultTheme } from "@chakra-ui/react"
+
+const theme = {
+  ...defaultTheme,
+}
+
+export default theme
---
+++ src/pages/index.js
@@ -0,0 +1,6 @@
+import React from "react"
+import { Button, Text } from "@chakra-ui/react"
+
+export default function Home() {
+  return <Button colorScheme="teal">asd</Button>
+}
