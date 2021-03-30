173237e88065adf148da075ee0b493f29636beb7
Binh Trinh <trinhthaibinh.ecom@gmail.com>
2021-03-30 16:00:27 -0700

    refactor project card component

--- src/components/HCard.js
+++ src/components/HCard.js
@@ -1,10 +1,21 @@
 import React from "react"
-import { Grid, Box } from "@chakra-ui/react"
+import { Grid, Box, VStack, Text, Icon, HStack, Button } from "@chakra-ui/react"
 import { motion } from "framer-motion"
+import { StaticImage } from "gatsby-plugin-image"
+import { FaGithub, FaLink } from "react-icons/fa"
+import ExternalLink from "./ExternalLink"
+import { Link } from "gatsby"
 
 const MotionBox = motion(Box)
 
-export default function HCard({ children }) {
+export default function HCard({
+  name,
+  description,
+  href,
+  github,
+  homepage,
+  technologies,
+}) {
   return (
     <MotionBox
       bg="#fff"
@@ -22,7 +33,80 @@ export default function HCard({ children }) {
         height="100%"
         gridGap="80px"
       >
-        {children}
+        <Box
+          display="flex"
+          flexDirection="column"
+          justifyContent="center"
+          paddingLeft="60px"
+        >
+          <Link href={href}>
+            <Text
+              fontWeight="bold"
+              fontSize="32px"
+              color="gray.600"
+              _after={{
+                content: '""',
+                display: "block",
+                height: "4px",
+                width: "60px",
+                backgroundColor: "blue.400",
+              }}
+            >
+              {name}
+            </Text>
+          </Link>
+          <Text my="12px">{description}</Text>
+          <Text color="gray.500">{technologies.join(" - ")} </Text>
+          <Link href={href}>
+            <Button variant="outline" mt="8px">
+              Read more
+            </Button>
+          </Link>
+          <HStack mt="16px" spacing="16px">
+            <ExternalLink href={github}>
+              <Icon
+                as={FaGithub}
+                width="28px"
+                height="28px"
+                _hover={{ color: "blue.400" }}
+                transition="all 500ms ease-out"
+                color="gray.600"
+              />
+            </ExternalLink>
+            <ExternalLink href={homepage}>
+              <Icon
+                as={FaLink}
+                width="28px"
+                height="28px"
+                _hover={{ color: "blue.400" }}
+                transition="all 500ms ease-out"
+                color="gray.600"
+              />
+            </ExternalLink>
+          </HStack>
+        </Box>
+        <Link href={href}>
+          <Box
+            display="flex"
+            justifyContent="flex-end"
+            alignItems="center"
+            bg="gray.200"
+            borderRadius="3xl"
+            _hover={{ bg: "yellow.200" }}
+            transition="all 0.5s ease-out"
+          >
+            <StaticImage
+              src="../assets/images/themoviedb.png"
+              alt="binh trinh"
+              placeholder="blurred"
+              layout="constrained"
+              width={550}
+              height={400}
+              quality={100}
+              style={{ borderRadius: "20px" }}
+            />
+          </Box>
+        </Link>
       </Grid>
     </MotionBox>
   )
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -18,8 +18,15 @@ export default function Projects() {
           <SectionSub>I build projects to showcase what I learned</SectionSub>
         </Box>
         <VStack spacing={8} mt={8}>
-          <HCard>
-            <Box
+          <HCard
+            name="theMovieDB"
+            description="Browse your favorite Movies, TV shows, and actors. Search for specific movies, shows or actors by date, rating, region and other categories. Browse the latest and greatest films and to find information about its actors..."
+            href="/blog/1"
+            github="https://github.com/binhthaitrinh/moviedb-2"
+            homepage="http://www.themoviedb.me/"
+            technologies={["React", "Redux", "TMBD API", "Sass"]}
+          >
+            {/* <Box
               display="flex"
               flexDirection="column"
               justifyContent="center"
@@ -45,7 +52,7 @@ export default function Projects() {
                 other categories. Browse the latest and greatest films and to
                 find information about its actors...
               </Text>
-              <Text color="gray.400">React - Redux - TMDB API - Sass </Text>
+              <Text color="gray.400">React - Redux - TMDB API - Sass  </Text>
               <Link href="/blog/123">
                 <Button variant="outline" mt="8px">
                   Read more
@@ -95,43 +102,7 @@ export default function Projects() {
                   style={{ borderRadius: "20px" }}
                 />
               </Box>
-            </Link>
-          </HCard>
-          <HCard>
-            <Box
-              display="flex"
-              justifyContent="flex-end"
-              alignItems="center"
-              bg="gray.200"
-              borderRadius="3xl"
-            >
-              <StaticImage
-                src="../assets/images/themoviedb.png"
-                alt="binh trinh"
-                placeholder="blurred"
-                layout="constrained"
-                width={550}
-                height={400}
-                quality={100}
-                style={{ borderRadius: "20px" }}
-              />
-            </Box>
-            <Box
-              display="flex"
-              flexDirection="column"
-              justifyContent="center"
-              paddingRight="60px"
-            >
-              <Text fontWeight="bold" fontSize="32px">
-                theMovieDB
-              </Text>
-              <Text>
-                Browse your favorite Movies, TV shows, and actors. Search for
-                specific movies, shows or actors by date, rating, region and
-                other categories. Browse the latest and greatest films and to
-                find information about its actors, crew, and reviews...
-              </Text>
-            </Box>
+            </Link> */}
           </HCard>
         </VStack>
       </Wrapper>
