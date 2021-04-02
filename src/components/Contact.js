92319947fbf5c6c65f66953e945fdf1f35f25d00
Binh Trinh <trinhthaibinh.ecom@gmail.com>
2021-04-01 22:54:03 -0700

    add contact section

---
+++ src/components/Contact.js
@@ -0,0 +1,79 @@
+import React from "react"
+import { Box, HStack, Text, Button, VStack, Icon } from "@chakra-ui/react"
+import { RiLinkedinFill, RiFacebookFill, RiGithubFill } from "react-icons/ri"
+import { Wrapper } from "./Wrapper"
+import Section from "./Section"
+import ExternalLink from "./ExternalLink"
+
+export default function Contact() {
+  return (
+    <Section>
+      <Wrapper>
+        <Box>
+          <HStack
+            bg="blue.700"
+            color="white"
+            borderRadius="2xl"
+            padding="30px 60px"
+            justifyContent="space-between"
+            alignItems="center"
+          >
+            <Box>
+              <Text fontSize="32px" fontWeight="bold" color="yellow.300">
+                Work Inquiry
+              </Text>
+              <Text>
+                If you like my work and want to connect, feel free to email me
+              </Text>
+              <Text>
+                I'm currently looking for either an internship or an entry level
+                web development position
+              </Text>
+            </Box>
+            <a href="mailto:trinhthaibinh.ecom@gmail.com">
+              <Button colorScheme="yellow">LET'S CHAT</Button>
+            </a>
+          </HStack>
+        </Box>
+        <Box>
+          <VStack spacing="60px" mt="60px" textAlign="center">
+            <Box>
+              <Text fontSize="32px" fontWeight="bold" mb="8px">
+                Stay Connected
+              </Text>
+              <Text color="gray.500">Fountain Valley, CA</Text>
+              <Text>trinhthaibinh.ecom@gmail.com</Text>
+            </Box>
+            <HStack spacing={8}>
+              <ExternalLink href="www.facebook.com/t2bkun">
+                <Box bg="blue.100" borderRadius="50%" padding="6px">
+                  <Icon
+                    as={RiFacebookFill}
+                    width="30px"
+                    height="30px"
+                    color="blue.600"
+                  />
+                </Box>
+              </ExternalLink>
+              <ExternalLink href="https://github.com/binhthaitrinh">
+                <Box bg="gray.200" borderRadius="50%" padding="6px">
+                  <Icon as={RiGithubFill} width="30px" height="30px" />
+                </Box>
+              </ExternalLink>
+              <ExternalLink href="https://www.linkedin.com/in/binhthaitrinh/">
+                <Box bg="blue.100" borderRadius="50%" padding="6px">
+                  <Icon
+                    as={RiLinkedinFill}
+                    width="30px"
+                    height="30px"
+                    color="blue.600"
+                  />
+                </Box>
+              </ExternalLink>
+            </HStack>
+          </VStack>
+        </Box>
+      </Wrapper>
+    </Section>
+  )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import Header from "../components/Header"
 import Projects from "../components/Projects"
 import Skills from "../components/Skills"
 import Blog from "../components/Blog"
+import Contact from "../components/Contact"
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
       <Projects />
       <Skills />
       <Blog />
+      <Contact />
     </>
   )
 }
